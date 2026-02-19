87ec9104fbffb0bd97aad1d2f88918e9c846839f
Mudassir006 <mudassir.cuiatd@gmail.com>
2026-02-19 10:02:13 +0300

    [ADD] replace the third party module with our custom for cc and bcc.

---
+++ pr_odoo_email_cc/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "PR Odoo Email CC/BCC",
+    "summary": "Add CC and BCC recipients in Odoo 17 email composer",
+    "version": "17.0.1.0.0",
+    "category": "Discuss",
+    "author": "Petroraq",
+    "license": "LGPL-3",
+    "depends": ["mail"],
+    "data": [
+        "views/compose_view.xml",
+        "views/mail_message_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
