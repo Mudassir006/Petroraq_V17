0eba4af7379004e097bc179b31e282b8d4e6afb0
Mudassir006 <95249069+Mudassir006@users.noreply.github.com>
2026-03-05 12:30:47 +0300

    Fix RFQ create crash and show custom lines in Products tab

--- pr_custom_purchase/models/purchase_requisition.py
+++ pr_custom_purchase/models/purchase_requisition.py
@@ -504,7 +504,6 @@ class PurchaseRequisition(models.Model):
                 "pr_name": pr.name,
                 "date_planned": pr.required_date,
                 "line_ids": [],
-                "order_line": [],
                 "date_request": pr.date_request,
                 "requested_by": pr.requested_by,
                 "department": pr.department,
@@ -521,14 +520,6 @@ class PurchaseRequisition(models.Model):
                     "unit": line.unit,
                     "price_unit": 0,
                     "cost_center_id": line.cost_center_id.id,
-                }))
-                rfq_vals["order_line"].append((0, 0, {
-                    "product_id": line.description.id,
-                    "name": line.description.display_name,
-                    "product_qty": line.quantity,
-                    "product_uom": line.description.uom_po_id.id or line.description.uom_id.id,
-                    "price_unit": line.unit_price,
-                    "date_planned": pr.required_date or fields.Date.context_today(self),
                 }))
 
             rfq = CustomRFQ.sudo().create(rfq_vals)
