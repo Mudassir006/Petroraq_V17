8e42034916d3aaacec0d4a099fa3d4fc19508424
Mudassir006 <mudassir.cuiatd@gmail.com>
2026-03-15 15:46:11 +0300

    [FIX] attendance calculation issue in payroll prediction

--- pr_hr_attendance/models/hr_payslip.py
+++ pr_hr_attendance/models/hr_payslip.py
@@ -74,10 +74,10 @@ class HrPayslip(models.Model):
                         'sequence': 35,
                         'number_of_days': rec.attendance_sheet_id.no_absence + num_weekend,
                         'number_of_hours': rec.attendance_sheet_id.tot_absence + (num_weekend * 8),
-                        'amount': rec.attendance_sheet_id.tot_absence_amount + weekend_amount + rec.attendance_sheet_id.carry_forward_deduction,
+                        'amount': rec.attendance_sheet_id.tot_absence_amount + weekend_amount + rec.attendance_sheet_id.carry_forward_absence_amount,
                     }]
                     rec.absence_num = rec.attendance_sheet_id.no_absence + num_weekend
-                    rec.total_absence = rec.attendance_sheet_id.tot_absence_amount + weekend_amount + rec.attendance_sheet_id.carry_forward_deduction
+                    rec.total_absence = rec.attendance_sheet_id.tot_absence_amount + weekend_amount + rec.attendance_sheet_id.carry_forward_absence_amount
                 else:
                     absence = [{
                         'name': "Absence",
@@ -86,10 +86,10 @@ class HrPayslip(models.Model):
                         'sequence': 35,
                         'number_of_days': rec.attendance_sheet_id.no_absence,
                         'number_of_hours': rec.attendance_sheet_id.tot_absence,
-                        'amount': rec.attendance_sheet_id.tot_absence_amount + rec.attendance_sheet_id.carry_forward_deduction,
+                        'amount': rec.attendance_sheet_id.tot_absence_amount + rec.attendance_sheet_id.carry_forward_absence_amount,
                     }]
                     rec.absence_num = rec.attendance_sheet_id.no_absence
-                    rec.total_absence = rec.attendance_sheet_id.tot_absence_amount + rec.attendance_sheet_id.carry_forward_deduction
+                    rec.total_absence = rec.attendance_sheet_id.tot_absence_amount + rec.attendance_sheet_id.carry_forward_absence_amount
 
                 late = [{
                     'name': "Late In",
