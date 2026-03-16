e394eabacdceaf91024ca1eb340a9e79ce1fe8d7
Mudassir006 <95249069+Mudassir006@users.noreply.github.com>
2026-03-16 13:10:00 +0300

    Fix SRN onchange crash for unsaved line ids

--- service_receipt_note/models/service_receipt.py
+++ service_receipt_note/models/service_receipt.py
@@ -303,11 +303,17 @@ class ServiceReceiptNoteLine(models.Model):
                 line.balance_qty = 0.0
                 continue
 
-            prior_done_lines = ReceiptLine.search([
+            domain = [
                 ("purchase_line_id", "=", line.purchase_line_id.id),
                 ("receipt_id.state", "=", "done"),
-                ("id", "!=", line.id),
-            ])
+            ]
+            # During onchange, unsaved one2many records may have a temporary NewId_* string,
+            # which cannot be compared against integer ids in SQL.
+            line_id = line._origin.id
+            if line_id:
+                domain.append(("id", "!=", line_id))
+
+            prior_done_lines = ReceiptLine.search(domain)
 
             already_received = sum(prior_done_lines.mapped("done_qty"))
             ordered = line.purchase_line_id.product_qty
