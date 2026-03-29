e7e16f25d0b131c104fd566ec6e514f5734fe0dd
Mudassir006 <95249069+Mudassir006@users.noreply.github.com>
2026-03-29 17:10:04 +0300

    Add professional time-based greeting in dashboard header

--- de_hr_workspace_attendance/static/src/js/approval_dashboard.js
+++ de_hr_workspace_attendance/static/src/js/approval_dashboard.js
@@ -3,6 +3,7 @@
 import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
+import { session } from "@web/session";
 
 class ApprovalDashboard extends Component {
     static template = "de_hr_workspace_attendance.ApprovalDashboard";
@@ -10,7 +11,7 @@ class ApprovalDashboard extends Component {
     setup() {
         this.orm = useService("orm");
         this.action = useService("action");
-        this.state = useState({ tiles: [], loading: true });
+        this.state = useState({ tiles: [], loading: true, userName: session.name || "" });
 
         onWillStart(async () => {
             await this.loadTiles();
@@ -32,6 +33,23 @@ class ApprovalDashboard extends Component {
 
     async refresh() {
         await this.loadTiles();
+    }
+
+
+    get salutation() {
+        const hour = new Date().getHours();
+        if (hour < 12) {
+            return "Good Morning";
+        }
+        if (hour < 17) {
+            return "Good Afternoon";
+        }
+        return "Good Evening";
+    }
+
+    get greetingText() {
+        const name = this.state.userName ? `Mr. ${this.state.userName}` : "";
+        return `${this.salutation}${name ? `, ${name}` : ""}`;
     }
 
     tileClass(tile) {
