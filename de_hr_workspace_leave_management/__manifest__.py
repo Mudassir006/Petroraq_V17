0e89a1cabede3976bcd25185cb878f4597d59062
Mudassir006 <95249069+Mudassir006@users.noreply.github.com>
2026-04-09 19:48:36 +0300

    Scope leave dashboard styles to custom action only

--- de_hr_workspace_leave_management/__manifest__.py
+++ de_hr_workspace_leave_management/__manifest__.py
@@ -16,9 +16,6 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'de_hr_workspace_leave_management/static/src/js/calendar_model.js',
-            'de_hr_workspace_leave_management/static/src/js/calendar_year_renderer.js',
-            'de_hr_workspace_leave_management/static/src/js/hooks.js',
             'de_hr_workspace_leave_management/static/src/js/emp_org_chart.js',
             'de_hr_workspace_leave_management/static/src/js/time_off_emp_card.js',
             'de_hr_workspace_leave_management/static/src/js/leave_management_dashboard_action.js',
