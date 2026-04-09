606c65541dfcd0878c439ab96c26051fd0ae8a9d
Mudassir006 <95249069+Mudassir006@users.noreply.github.com>
2026-04-09 14:30:12 +0300

    Restore upcoming holidays in leave department tile

--- hr_leave_dashboard/static/src/js/time_off_emp_dashboard.js
+++ hr_leave_dashboard/static/src/js/time_off_emp_dashboard.js
@@ -5,7 +5,6 @@ import { TimeOffCard } from '@hr_holidays/dashboard/time_off_card';
 import { TimeOffEmpCard } from './time_off_emp_card';
 import { TimeOffEmpOrgChart } from './emp_org_chart';
 import { EmpDepartmentCard } from './time_off_emp_card';
-import { ApprovalStatusCard } from './time_off_emp_card';
 import { Component, onWillStart, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 
@@ -31,7 +30,6 @@ patch(TimeOffDashboard.prototype,{
             data: {}
 
         })
-
         onWillStart(async() => {
             await this.userService.hasGroup('hr_holidays.group_hr_holidays_manager').then(hasGroup => {
                 this.manager = hasGroup;
@@ -77,26 +75,6 @@ patch(TimeOffDashboard.prototype,{
                 }
             }
         );
-         this.approval_status_count = await this.orm.call(
-            'hr.leave',
-            'get_approval_status_count',
-            [this.currentEmployee.data.id],
-            {
-                context: {
-                    employee_id: this.props.employeeId
-                }
-            }
-        );
-          this.all_validated_leaves = await this.orm.call(
-            'hr.leave',
-            'get_all_validated_leaves',
-            [],
-            {
-                context: {
-                    employee_id: this.props.employeeId
-                }
-            }
-        );
         if (this.props.employeeId == null) {
             this.props.employeeId = this.currentEmployee.data.id;
         }
@@ -104,4 +82,4 @@ patch(TimeOffDashboard.prototype,{
         })
     },
 });
-TimeOffDashboard.components = { ...TimeOffDashboard.components, TimeOffCard, TimeOffEmpCard ,TimeOffEmpOrgChart, EmpDepartmentCard, ApprovalStatusCard};
+TimeOffDashboard.components = { ...TimeOffDashboard.components, TimeOffCard, TimeOffEmpCard ,TimeOffEmpOrgChart, EmpDepartmentCard};
